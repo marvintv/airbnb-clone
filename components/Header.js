56428cf5fdba7f6f5cc5e8a15fe7f0038f031fc7
Marvin <marvinvilaysack@gmail.com>
2021-12-27 01:01:55 -0600

    search bar

--- components/Header.js
+++ components/Header.js
@@ -11,14 +11,15 @@ import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 
 import { DateRangePicker } from 'react-date-range';
+import { useRouter } from 'next/router';
 
 
-const Header = () => {
+const Header = ({placeholder}) => {
     const [searchInput, setSearchInput] = useState("");
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setEndDate] = useState(new Date());
     const [numberOfGuests, setNumberOfGuests] = useState(1);
-
+    const router = useRouter();
 
     const handleSelect = (ranges) => {
         console.log(ranges)
@@ -34,20 +35,31 @@ const Header = () => {
     const resetInput = () => {
         setSearchInput("")
     }
+    const search = () => {
+        router.push({
+            pathname: '/search',
+            query: {
+                location: searchInput,
+                startDate: startDate.toISOString(),
+                endDate: endDate.toISOString(),
+                numberOfGuests
+            }
+        })
+    }
     return (
 
         // sticky, navbar stuck to page, stick to top
         <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
            
             {/* left */}
-            <div className="relative flex items-center h-10 cursor-pointer my-auto">
+            <div onClick={() => router.push("/")} className="relative flex items-center h-10 cursor-pointer my-auto">
                <Image alt="logo" src="https://links.papareact.com/qd3" objectFit="contain" objectPosition="left"  layout="fill"/>
             </div>
 
             {/* middle search */}
             <div className="flex flex-row items-center md:border-2 rounded-full py-2 md:shadow-sm">
 
-                <input value={searchInput} onChange={(e)=> setSearchInput(e.target.value)} className="flex-grow pl-5 bg-transparent outline-none text-small text-gray-600 placeholder-gray-400" type="text" placeholder="Start Your Search"/>
+                <input value={searchInput} onChange={(e)=> setSearchInput(e.target.value)} className="flex-grow pl-5 bg-transparent outline-none text-small text-gray-600 placeholder-gray-400" type="text" placeholder={placeholder || "Start Your Search"}/>
                 <SearchIcon className="h-8 bg-red-400 text-white rounded-full p-2 mr-2 cursor-pointer hidden md:inline-flex md:mx-2"/>
             </div>
 
@@ -57,7 +69,7 @@ const Header = () => {
 
                 {/* inline when medium. show on medium */}
                 <p className="hidden md:inline cursor-pointer">Become a Host</p>
-                <GlobeAltIcon className="h-6 space-x-2 cursor-pointer"/>
+                <GlobeAltIcon className="h-6 space-x-2 cursor-pointer hidden md:inline-flex"/>
 
                 <div className="flex items-center space-x-2 p-2 rounded-full border-2">
                     <MenuIcon className="h-6"/>
@@ -82,7 +94,7 @@ const Header = () => {
                 <div className="flex">
                     {/* emmet for react */}
                     <button onClick={resetInput} className="flex-grow text-gray-500">Cancel</button>
-                    <button className="flex-grow text-red-400">Search</button>
+                    <button onClick={search} className="flex-grow text-red-400">Search</button>
                 </div>
             </div>
         }
