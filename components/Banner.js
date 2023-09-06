e5115c4a735d7f536ea180b5f5797d0d263cd8ce
Marvin <marvinvilaysack@gmail.com>
2023-09-05 22:58:37 -0700

    adding airbnb clone files

--- components/Banner.js
+++ components/Banner.js
@@ -14,7 +14,7 @@ const Banner = () => {
                 
                 <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl
                 active:scale-90 transition duration-150
-                ">I'm flexible</button>
+                ">Choose a place</button>
             </div>
         </div>
     )
